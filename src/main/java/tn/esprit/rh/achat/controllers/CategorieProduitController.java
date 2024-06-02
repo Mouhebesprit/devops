d0a0f310c42fb0828966834e0ff77c430409a919
Mouhebesprit <mouheb.benelwafi@esprit.tn>
2024-06-03 00:18:17 +0100

    trying to trigger webhook

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -23,6 +23,7 @@ public class CategorieProduitController {
 		List<CategorieProduit> list = categorieProduitService.retrieveAllCategorieProduits();
 		return list;
 	}
+	//M
 	//trriger
 	//sss
 	//ss
